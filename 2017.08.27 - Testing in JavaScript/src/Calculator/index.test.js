c93fae3b2554f9bf092ed31bf1d3793e4cc92037
Radoslav Stankov <rstankov@gmail.com>
2017-12-21 11:18:11 +0200

    Extract test helper

--- 2017.08.27 - Testing in JavaScript/src/Calculator/index.test.js
+++ 2017.08.27 - Testing in JavaScript/src/Calculator/index.test.js
@@ -3,11 +3,13 @@ import Component from './index';
 import { shallow, mount } from 'enzyme';
 import Memory from './Memory';
 
+const dt = s => `[data-test="${s}"]`
+
 describe(Component.name, () => {
   it('renders memory', () => {
     const component = mount(<Component />);
 
-    expect(component.find('[data-test="result"]')).toIncludeText('0');
+    expect(component.find(dt('result'))).toIncludeText('0');
   });
 
   describe('actions', () => {
@@ -16,31 +18,31 @@ describe(Component.name, () => {
     beforeEach(() => {
       component = mount(<Component />);
 
-      component.find('[data-test="1"]').simulate('click');
-      component.find('[data-test="+"]').simulate('click');
-      component.find('[data-test="1"]').simulate('click');
+      component.find(dt('1')).simulate('click');
+      component.find(dt('+')).simulate('click');
+      component.find(dt('1')).simulate('click');
     });
 
     it('can add digits and operands', () => {
-      expect(component.find('[data-test="result"]')).toIncludeText('1 + 1');
+      expect(component.find(dt('result'))).toIncludeText('1 + 1');
     });
 
     it('can sum numbers', () => {
-      component.find('[data-test="calculate"]').simulate('click');
+      component.find(dt('calculate')).simulate('click');
 
-      expect(component.find('[data-test="result"]')).toIncludeText('2');
+      expect(component.find(dt('result'))).toIncludeText('2');
     });
 
     it('can remove digits and operands', () => {
-      component.find('[data-test="remove"]').simulate('click');
+      component.find(dt('remove')).simulate('click');
 
-      expect(component.find('[data-test="result"]')).toIncludeText('1 +');
+      expect(component.find(dt('result'))).toIncludeText('1 +');
     });
 
     it('can be reset', () => {
-      component.find('[data-test="reset"]').simulate('click');
+      component.find(dt('reset')).simulate('click');
 
-      expect(component.find('[data-test="result"]')).toIncludeText('0');
+      expect(component.find(dt('result'))).toIncludeText('0');
     });
   });
 });
